e43b15cd58e5f65975c142ab4ffec25ebc92f2ea
Kaif Chaudhary <kaifchgee@gmail.com>
2026-04-08 09:51:34 +0500

    Update developer credits in menu text

--- wasiplugins/menu.js
+++ wasiplugins/menu.js
@@ -38,7 +38,7 @@ module.exports = {
             menuText += `╰─────────────────\n\n`;
         }
         
-        menuText += `> _Developed by Mr Wasi (ixxwasi)_`;
+        menuText += `> _Developed by Kaif x Chaudhary and Mr Wasi_`;
 
         await wasi_sock.sendMessage(wasi_origin, { 
             text: menuText,
